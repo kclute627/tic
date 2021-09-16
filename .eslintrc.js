b2b09ee8a34f1c64e0ad68c1a9b65f4ad993e99a
Kyle <you@example.com>
2021-09-16 14:51:56 -0500

    test

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    env: {
+        node: true,
+        es2021: true,
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier",
+        "prettier/@typescript-eslint",
+        "prettier/react"
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: "module",
+    },
+    plugins: ["react", "@typescript-eslint"],
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
+    rules: {
+        "@typescript-eslint/no-unused-vars": "off",
+    },
+};
